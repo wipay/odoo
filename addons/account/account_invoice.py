b6e78e7eae52347fbe6c9c1c414dd26b5d31d0a0
José Miguel <jose.rivero@trescloud.com>
2016-05-30 19:56:29 -0500

    FIX

--- addons/account/account_invoice.py
+++ addons/account/account_invoice.py
@@ -1397,10 +1397,11 @@ class account_invoice_line(osv.osv):
         tax_obj = self.pool.get('account.tax')
         cur_obj = self.pool.get('res.currency')
         for line in self.browse(cr, uid, ids):
-            if line.invoice_id.type in ('out_invoice','in_invoice'):
-                context.update({'document_date': line.invoice_id.date_invoice})
-            else:
-                context.update({'document_date': line.invoice_id.invoice_rectification_id.date_invoice})
+            if line.invoice_id:
+                if line.invoice_id.type in ('out_invoice','in_invoice'):
+                    context.update({'document_date': line.invoice_id.date_invoice})
+                else:
+                    context.update({'document_date': line.invoice_id.invoice_rectification_id.date_invoice})
             price = line.price_unit * (1-(line.discount or 0.0)/100.0)
             taxes = tax_obj.compute_all(cr, uid, line.invoice_line_tax_id, price, line.quantity, product=line.product_id, partner=line.invoice_id.partner_id, context=context)
             res[line.id] = taxes['total']
