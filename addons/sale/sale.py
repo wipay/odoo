f1969556c9841233e2b940a4c081ce7f33080531
José Miguel <jose.rivero@trescloud.com>
2017-04-14 09:16:35 -0500

    Mal definido cursor y uid

--- addons/sale/sale.py
+++ addons/sale/sale.py
@@ -99,7 +99,7 @@ class sale_order(osv.osv):
             context.update({'document_date': order.date_order.split(' ')[0],'force_vat': order.force_vat})
             #Código modificado por TRESCLOUD para evitar error en caso que el módulo law_of_solidarity no este instalado
             solidarity_compensation = 0.0
-            module_ids = self.pool.get('ir.module.module').search(self.cr, self.uid, [('name','=','law_of_solidarity'), ('state','=','installed')], context=context)
+            module_ids = self.pool.get('ir.module.module').search(cr, uid, [('name','=','law_of_solidarity'), ('state','=','installed')], context=context)
             if module_ids:
                 solidarity_compensation = order.solidarity_compensation
             res[order.id] = {
