c5f7181e45f64b389db82dc9523086b8700916f4
Santiago Orozco <santiago.orozco@trescloud.com>
2017-05-08 15:47:07 -0500

    [FIX] stock: Contexto cuando se realiza la copia.

--- addons/stock/wizard/stock_return_picking.py
+++ addons/stock/wizard/stock_return_picking.py
@@ -211,7 +211,7 @@ class stock_return_picking(osv.osv_memory):
                                             'location_dest_id': move.location_id.id,
                                             'date': date_cur,
                                             'prodlot_id': data_get.prodlot_id.id,
-                })
+                }, context=context)
                 move_obj.write(cr, uid, [move.id], {'move_history_ids2':[(4,new_move)]}, context=context)
         if not returned_lines:
             raise osv.except_osv(_('Warning!'), _("Please specify at least one non-zero quantity."))
