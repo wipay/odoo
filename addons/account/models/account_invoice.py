c0e59600a47ffdfd84a01ca1befc10ab1fd3716e
Santiago Orozco <santiago.orozco@trescloud.com>
2019-03-29 15:38:50 -0500

    [IMP][account]: Nuevo tipo de factura hr.advance

--- addons/account/models/account_invoice.py
+++ addons/account/models/account_invoice.py
@@ -204,6 +204,7 @@ class AccountInvoice(models.Model):
             ('in_invoice','Vendor Bill'),
             ('out_refund','Customer Refund'),
             ('in_refund','Vendor Refund'),
+            ('hr_advance','Adelantos Empleados'),
         ], readonly=True, index=True, change_default=True,
         default=lambda self: self._context.get('type', 'out_invoice'),
         track_visibility='always')
