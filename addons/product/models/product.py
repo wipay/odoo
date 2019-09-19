21ef634cc68761e279e099c9115c6b756554ba22
Santiago Orozco <santiago.orozco@trescloud.com>
2019-09-18 19:57:25 -0500

    [FIX][producto]: Comentarios del efecto en ecommerce

--- addons/product/models/product.py
+++ addons/product/models/product.py
@@ -288,6 +288,15 @@ class ProductProduct(models.Model):
 
     @api.one
     def _set_image_value(self, value):
+        # Para el sitio web (ecommerce) produce este cambio los siguientes efectos:
+        # 1. Sin variantes de productos: Las imagenes son mostradas del producto template.
+        # es decir, nunca se pierde la imagen
+        # 2. Con Variantes de productos: Las imagenes toman los siguientes valores.
+        # 2.a Si es solo una variante, el sistema actua como si no tuviera variantes.
+        # 2.b Si hay mas de una, en el ecommerce se muestra la imagen del product.template
+        # en primera instancia, y luego en las variantes, se muestra la foto respectiva si
+        # han sido configuradas, caso contrario, se muestra la del product.template.
+        # Esto asegura consistencia en las imagenes
         image = tools.image_resize_image_big(value)
         ref = self.env.ref
         # Verificamos si es que esta configurado variantes de producto en la configuracion de
@@ -300,7 +309,8 @@ class ProductProduct(models.Model):
             self.product_tmpl_id.image = image
         else:
             self.image_variant = image
-            if not len(self.product_tmpl_id.product_variant_ids) == 1:
+            # Una sola variante una sola foto en todas partes
+            if len(self.product_tmpl_id.product_variant_ids) == 1:
                 self.image_variant = False # Nunca se setea la imagen de la variante cuando no esta configurada la opcion
                 self.product_tmpl_id.image = image
             if not self.product_tmpl_id.image:
