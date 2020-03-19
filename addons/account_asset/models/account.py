6a463a534bed4d1a4ee9a2b40a5f3105c92e0007
jmrivero <jose.rivero@trescloud.com>
2020-03-18 20:44:39 -0400

    Problea con la aprobacion de fact y pagos relacionados con permisos de activos

--- addons/account_asset/models/account.py
+++ addons/account_asset/models/account.py
@@ -12,13 +12,19 @@ class AccountMove(models.Model):
     @api.multi
     def button_cancel(self):
         for move in self:
-            for line in move.asset_depreciation_ids:
+            #Codigo moldificado por Trescloud pues usuarios contables que no tienen acceso
+            #al modulo de activos no le permite aprobar o anular fact, nc, pagos, etc, por
+            #tal motivo se agrega el sudo()
+            for line in move.sudo().asset_depreciation_ids:
                 line.move_posted_check = False
         return super(AccountMove, self).button_cancel()
 
     @api.multi
     def post(self):
         for move in self:
-            for depreciation_line in move.asset_depreciation_ids:
+            #Codigo moldificado por Trescloud pues usuarios contables que no tienen acceso
+            #al modulo de activos no le permite aprobar o anular fact, nc, pagos, etc, por
+            #tal motivo se agrega el sudo()
+            for depreciation_line in move.sudo().asset_depreciation_ids:
                 depreciation_line.post_lines_and_close_asset()
         return super(AccountMove, self).post()
