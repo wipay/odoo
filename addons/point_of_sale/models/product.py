f40d857c181536a15ce7a9ef1ae763088b6b65ee
Andres Calle <andres.calle@trescloud.com>
2020-09-23 02:01:25 -0500

    Removidos cambios innecesarios

--- addons/point_of_sale/models/product.py
+++ addons/point_of_sale/models/product.py
@@ -17,8 +17,7 @@ class ProductTemplate(models.Model):
         product_ctx = dict(self.env.context or {}, active_test=False)
         if self.with_context(product_ctx).search_count([('id', 'in', self.ids), ('available_in_pos', '=', True)]):
             if self.env['pos.session'].sudo().search_count([('state', '!=', 'closed')]):
-                pass
-                #raise UserError(_('You cannot delete a product saleable in point of sale while a session is still opened.'))
+                raise UserError(_('You cannot delete a product saleable in point of sale while a session is still opened.'))
         return super(ProductTemplate, self).unlink()
 
     @api.onchange('sale_ok')
