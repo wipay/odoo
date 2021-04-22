e7744b9bc74ec5dd26aa3b04d8656febfbaa5e80
Reinier Quevedo Batista <reisyreinier@gmail.com>
2021-04-22 02:40:17 -0400

    [IMP] l10n_ec: Se crea el metodo model de validacion de ruc y cedula para poder usar con valores planos y no con modelo partner.
    Change-Id: I4c2e0dc185250525b3898467bd3cac1ac42d6c61

--- addons/l10n_ec/models/res_partner.py
+++ addons/l10n_ec/models/res_partner.py
@@ -1,23 +1,31 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import models, _
+from odoo import models, _, api
 from stdnum.ec import ci, ruc
 from odoo.exceptions import ValidationError
+
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     def check_vat_ec(self, vat):
-        if self.l10n_latam_identification_type_id.is_vat:
+        return self.model_check_vat_ec(vat, self.l10n_latam_identification_type_id.id)
+
+    @api.model
+    def model_check_vat_ec(self, vat, l10n_latam_identification_type_id):
+        vat_type = self.env['l10n_latam.identification.type'].browse(l10n_latam_identification_type_id)
+        if not vat_type:
+            raise ValidationError(_('Identification type is required in order to validate VAT'))
+        if vat_type.is_vat:
             ruc_vat_type = self.env.ref('l10n_ec.ec_ruc')
             ced_vat_type = self.env.ref('l10n_ec.ec_dni')
-            if self.l10n_latam_identification_type_id in (ruc_vat_type,ced_vat_type):
-                #temporal fix as stdnum.ec is allowing old format with a dash in between the number                    
-                if not self.vat.isnumeric():
+            if vat_type in (ruc_vat_type, ced_vat_type):
+                # temporal fix as stdnum.ec is allowing old format with a dash in between the number
+                if not vat.isnumeric():
                     raise ValidationError(_('Ecuadorian VAT number must contain only numeric characters'))
-            if self.l10n_latam_identification_type_id == ced_vat_type:
+            if vat_type == ced_vat_type:
                 return ci.is_valid(vat)
-            elif self.l10n_latam_identification_type_id == ruc_vat_type and vat != '9999999999999':
+            elif vat_type == ruc_vat_type and vat != '9999999999999':
                 return ruc.is_valid(vat)
         return True
 
